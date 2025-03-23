e52c9e2da3726ce5cafe4367c7ee00f83f62d035
moisemb <papemoussa.mbengue.11@gmail.com>
2025-03-23 04:49:56 +0000

    Update Swagger UI to version 5.20.1 and configure static asset serving in main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,23 +2,33 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // Serve static files
+  app.useStaticAssets(join(__dirname, '..', 'node_modules/swagger-ui-dist'), {
+    prefix: '/api-doc',
+  });
 
   const config = new DocumentBuilder()
     .setTitle('Eureka-Learn API')
     .setDescription('Documentation API of Eureka-Learn')
     .setVersion('1.0')
-    .addBearerAuth()  // Add this line for bearer token authentication
+    .addBearerAuth()
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api-doc', app, document, {
-    swaggerOptions: {
-      persistAuthorization: true,
-    },
-    customSiteTitle: 'Eureka-Learn API Documentation'
+    customSiteTitle: 'Eureka-Learn API Documentation',
+    customfavIcon: '/api-doc/favicon-32x32.png',
+    customJs: [
+      '/api-doc/swagger-ui-bundle.js',
+      '/api-doc/swagger-ui-standalone-preset.js',
+    ],
+    customCssUrl: '/api-doc/swagger-ui.css',
   });
 
   const corsOptions: CorsOptions = {
@@ -34,7 +44,7 @@ async function bootstrap() {
     ],
     exposedHeaders: ['Content-Range', 'Authorization'],
   };
-  
+
   app.enableCors(corsOptions);
   await app.listen(3002);
 }
