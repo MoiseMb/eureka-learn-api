e64d189e6a9f5eaa1af279fc3aad16c5c8ccb592
moisemb <papemoussa.mbengue.11@gmail.com>
2025-03-24 00:33:00 +0000

    fix probleme to create submission

--- src/submission/dto/create-submission.dto.ts
+++ src/submission/dto/create-submission.dto.ts
@@ -6,23 +6,17 @@ export class CreateSubmissionDto {
         description: 'The URL of the submitted file',
         example: 'https://example.com/file.pdf',
     })
-    @IsNotEmpty()
-    @IsString()
     fileUrl: string;
 
     @ApiProperty({
         description: 'The ID of the student submitting the file',
         example: 1,
     })
-    @IsNotEmpty()
-    @IsNumber()
     studentId: number;
 
     @ApiProperty({
         description: 'The ID of the subject for which the file is submitted',
         example: 1,
     })
-    @IsNotEmpty()
-    @IsNumber()
     subjectId: number;
 }
